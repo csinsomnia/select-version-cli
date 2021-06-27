7fc8f589e39d2a779eaa27eba338c45bee4a2840
keyell <408293432@qq.com>
2021-06-27 19:53:21 +0800

    支持preid

--- src/selectVersion.js
+++ src/selectVersion.js
@@ -6,7 +6,7 @@ const semver = require('semver')
 // 不要使用 stdout，参考链接https://github.com/SBoudrias/Inquirer.js/issues/519
 const prompt = inquirer.createPromptModule({ output: process.stderr })
 
-function getReleaseVersion() {
+function getReleaseVersion(preid) {
   return new Promise((resolve, reject) => {
     const dir = path.resolve(process.cwd(), 'package.json')
     if (!fs.existsSync(dir)) {
@@ -14,17 +14,20 @@ function getReleaseVersion() {
       return
     }
     const version = require(dir).version
+    const name = require(dir).name // 包名
     if (!version) {
       reject(new Error('no version in package.json'))
       return
     }
     const releaseType = ['patch', 'minor', 'major', 'prepatch', 'preminor', 'premajor', 'prerelease']
-    const choices = releaseType.map(item => `${item}: ${semver.inc(version, item)}`)
+    const choices = releaseType.map(item => `${item}: ${semver.inc(version, item, preid)}`)
+    const chalk = require('chalk')
     prompt([
       {
         name: 'version',
         type: 'list',
-        message: 'Select release version',
+        // message: 'Select release version',
+        message: `${chalk.redBright(name)} 包当前版本为: ${chalk.red(version)}, 请选择新版本号`,
         choices: choices
       }
     ]).then(({ version }) => {
